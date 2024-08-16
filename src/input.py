c1c8af221a30f33922b5ee42149a9e6a1760fa11
Tabea Röthemeyer <tabea.roethemeyer@gruppe.ai>
2024-08-16 10:05:39 +0200

    fix(core): handle incorrect user input
    - Allow the user to re-enter the input if it is not parsable to int
    - Before, the program would crash if the user entered a non-integer value

--- src/input.py
+++ src/input.py
@@ -1,7 +1,12 @@
 def get_number_from_input(message:str = "Enter a number: ")-> int:
     """
-    Get a number from the user input
+    Get a number from the user input. If the input is not a number, 
+    it will continue to ask the user to enter a valid number.
     :return: int
     """
-    number = input(message)
-    return int(number)
+    while True:
+        try:
+            number = int(input(message))
+            return number
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
