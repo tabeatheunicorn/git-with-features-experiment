e0131790940a4fde590afb23c515700a23daa792
Tabea Röthemeyer <tabea.roethemeyer@gruppe.ai>
2024-08-16 09:56:23 +0200

    Refactor(core): Allow custom input prompt
    - Add a new parameter to the `input` function to allow custom input prompt
    - Useful if the user should be displayed context information for the number they are entering

--- src/input.py
+++ src/input.py
@@ -1,7 +1,7 @@
-def get_number_from_input()-> int:
+def get_number_from_input(message:str = "Enter a number: ")-> int:
     """
     Get a number from the user input
     :return: int
     """
-    number = input("Enter a number: ")
+    number = input(message)
     return int(number)
