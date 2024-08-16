25b20245c857590ead4bc520f5539a290de7e1bb
Tabea Röthemeyer <tabea.roethemeyer@gruppe.ai>
2024-08-16 10:01:04 +0200

    feat(add): call add with user input
    - Integrate user input and add function into calculator program
    - Test that the program works as expected by running it
    - Useful input works, but invalid input outputs errors

--- src/main.py
+++ src/main.py
@@ -1,5 +1,14 @@
+from operator import add
+from input import get_number_from_input
+
+
 def calculator_main():
-    print("I want to become a calculator")
+    print("I can calculate")
+    print("Let's start with addition")
+    a = get_number_from_input("Enter the first number: ")
+    b = get_number_from_input("Enter the second number: ")
+    result = add(a, b)
+    print(f"The result of {a} + {b} is {result}")
     
 if __name__ == "__main__":
     calculator_main()
