a5f7aba8410a5bdbde5b7ca394160fc24debf1a1
Jane Doe <jane.doe@example.com>
2024-09-09 23:59:59 +0200

    feat(op-select): add datatype for named operation
    - create NamedTuple datatype
    - intended to store all operations

--- src/operations.py
+++ src/operations.py
@@ -1,2 +1,8 @@
+from typing import Callable, NamedTuple
+
+class Operation(NamedTuple):
+    name: str
+    func: Callable[[int, int], int]
+
 def add(a: float|int, b: float|int) -> float|int:
     return a + b
