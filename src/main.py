c9cabb6d8723adb3c9f5bae3b5e9a6084d4f0df0
Jane Doe <jane.doe@example.com>
2024-09-10 00:34:16 +0200

    feat(core, op-select): integrate op-select in main
    - User can now select the operation to perform
    - Output contains operation and result on terminal

--- src/main.py
+++ src/main.py
@@ -1,14 +1,15 @@
 from operator import add
 from input import get_number_from_input
+from operations import get_operation_from_user
 
 
 def calculator_main():
     print("I can calculate")
-    print("Let's start with addition")
     a = get_number_from_input("Enter the first number: ")
     b = get_number_from_input("Enter the second number: ")
-    result = add(a, b)
-    print(f"The result of {a} + {b} is {result}")
+    
+    operation = get_operation_from_user()
+    print(f"The result of {operation.name} with {a}, {b} is {operation.func(a, b)}")
     
 if __name__ == "__main__":
     calculator_main()
