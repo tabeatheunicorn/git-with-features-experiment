1d6179df58fca7ed870c89b73242e364b2e94a32
Jane Doe <jane.doe@example.com>
2024-09-10 00:02:32 +0200

    feat(op-select): implement operation selection
    - display all options on terminal to user
    - allow index-based selection of operations by user
    - start list of operations

--- src/operations.py
+++ src/operations.py
@@ -6,3 +6,26 @@ class Operation(NamedTuple):
 
 def add(a: float|int, b: float|int) -> float|int:
     return a + b
+
+
+operations = [
+    Operation(name="Add", func=add),
+    Operation(name="Subtract", func=add)
+]
+    
+def get_operation_from_user() -> Operation:
+    print("Available operations:")
+    for idx, operation in enumerate(operations, 1):
+        print(f"{idx}. {operation.name}")
+    
+    while True:
+        try:
+            operation_id = int(input("Enter an operation id: ")) - 1
+            if 0 <= operation_id < len(operations):
+                result = operations[operation_id]
+                print(f"Selected operation: {result.name}")
+                return result
+            else:
+                print("Invalid ID. Please select a valid operation.")
+        except ValueError:
+            print("Please enter a valid number.")
