5cb46304be0eb824f549f5e215e106b9d5cc5ef4
Jane Doe <jane.doe@example.com>
2024-09-09 23:13:39 +0200

    feat(subtract): integrate subtract func in main
    - Import operator and input dependency
    - Assume two numbers should be subtracted

--- src/main.py
+++ src/main.py
@@ -1,5 +1,13 @@
+from input import get_number_from_input
+from operations import subtract
+
+
 def calculator_main():
-    print("I want to become a calculator")
+    print("Let's subtract two numbers!")
+    a = get_number_from_input("Enter the first number: ")
+    b = get_number_from_input("Enter the second number: ")
+    result = subtract(a, b)
+    print(f"The result of {a} - {b} is {result}")
     
 if __name__ == "__main__":
     calculator_main()
